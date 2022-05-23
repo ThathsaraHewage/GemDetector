a181e0d5bc2f7f3fb75e4dd737747477c724c4d2
HP <thathsara.hewage99@gmail.com>
2022-05-23 23:55:13 +0530

    splash screen and camera,image selecting options

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'animation_screen.dart';
+import 'Home/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Raindrop App',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: Stack(
+            children: <Widget>[
+              Scaffold(
+                  body: Home()
+              ),
+              IgnorePointer(
+                  child: AnimationScreen(
+                      color: Theme.of(context).accentColor
+                  )
+              )
+            ]
+        )
+    );
+  }
+}
+
